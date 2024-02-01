dd80b5b5eefa85214a920f1fd6854b9f0f791b54
Dylan Hobbs <dylan.hobbs@vouched.id>
2024-02-01 01:11:08 -0600

    fixup - remove from public routes

--- middleware.ts
+++ middleware.ts
@@ -3,19 +3,14 @@ import { authMiddleware } from "@clerk/nextjs";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({
+export default authMiddleware();
+
+export const config = {
   publicRoutes: [
     "/api/uploadthing",
     "/api/ai/",
-    "/api/notification",
     "/_vercel/speed-insights/vitals",
-    "/api/notification",
-    "/api/sendEmail",
-    "/api/serverless-example",
   ],
-});
-
-export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
   ignoredRoutes: ["/api/sendEmail", "/api/notification"],
 };
